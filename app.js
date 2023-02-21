dae85bbb3965d6ef2a5201b161a015de79775137
TheDanishMexican <danieljchristjansen@gmail.com>
2023-02-21 13:48:19 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let number = 0;
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("start works");
+  document.querySelector("#btn-increment").addEventListener("click", addNumber);
+  document
+    .querySelector("#btn-decrement")
+    .addEventListener("click", subtractNumber);
+}
+
+function addNumber() {
+  console.log("add works");
+  number = number + 1;
+  updateNumber();
+}
+
+function subtractNumber() {
+  console.log("subtract works");
+  number = number - 1;
+  updateNumber();
+}
+
+function updateNumber() {
+  document.querySelector("#number").textContent = number;
+}
